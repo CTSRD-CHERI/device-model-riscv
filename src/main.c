a53d2419753cb5d2bafdb550caca8a8f55636480
Ruslan Bukin <br@bsdpad.com>
2021-08-25 09:43:02 +0100

    Purecap support added to the initialization code.

--- src/main.c
+++ src/main.c
@@ -34,7 +34,8 @@
 #include <sys/sem.h>
 #include <sys/list.h>
 #include <sys/smp.h>
-#include <sys/tick.h>
+#include <sys/cheri.h>
+#include <sys/io.h>
 
 #include <mips/beri/beri_epw.h>
 
@@ -48,30 +49,47 @@
 #include <app/fpu_test/fpu_test.h>
 #include <app/callout_test/callout_test.h>
 
+#ifdef __CHERI_PURE_CAPABILITY__
+#include <cheri_init_globals.h>
+#endif
+
 #include "board.h"
 #include "device-model.h"
+#include "main.h"
 
 static struct epw_softc epw_sc;
+
+extern void *pvAlmightyDataCap;
+
+#ifdef __CHERI_PURE_CAPABILITY__
+void
+start_purecap(void)
+{
+
+	cheri_init_globals_3( __builtin_cheri_global_data_get(),
+		__builtin_cheri_program_counter_get(),
+		__builtin_cheri_global_data_get());
+}
+#endif
 
 int
 main(void)
 {
+	capability base, window;
+	capability cap;
 
 	printf("%s: starting on hart %d\n", __func__, PCPU_GET(cpuid));
 
-	capability base, window;
-	base = (capability)0x50000000;
-	window = (capability)0x60000000;
+	cap = pvAlmightyDataCap;
+	base = mdx_setoffset(cap, 0x50000000);
+	window = mdx_setoffset(cap, 0x60000000);
 
-printf("%s 1\n", __func__);
 	epw_init(&epw_sc, base, window);
-printf("%s 2\n", __func__);
+
 	/* Enable EPW */
 	epw_control(&epw_sc, 1);
-printf("%s 3\n", __func__);
 
 	dm_init(&epw_sc);
-printf("%s 4\n", __func__);
 	dm_loop(&epw_sc);
 
 	panic("dm_loop returned");
@@ -90,4 +108,15 @@ printf("%s 4\n", __func__);
 	panic("reached unreachable place");
 
 	return (0);
+}
+
+/* Also provide a simple purecap strlen function */
+__SIZE_TYPE__ strlen(const char* str)
+{
+	const char* p = str;
+
+	while (*p != '\0')
+		p++;
+
+	return p - str;
 }
